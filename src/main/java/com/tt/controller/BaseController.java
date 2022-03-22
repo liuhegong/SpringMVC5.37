8ce16a31d6d0dce55de0f85f59ce03e1882e08c1
liuhegong <liuhegong@gmail.com>
2022-03-23 00:47:04 +0800

    Initial commit

---
+++ src/main/java/com/tt/controller/BaseController.java
@@ -0,0 +1,5 @@
+package com.tt.controller;
+
+public class BaseController {
+
+}
---
+++ src/main/java/com/tt/controller/system/SystemController.java
@@ -0,0 +1,20 @@
+package com.tt.controller.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tt.controller.BaseController;
+
+@Controller
+public class SystemController{
+
+	@GetMapping("/hello")
+	@ResponseBody
+    public String handle(Model model) {
+        model.addAttribute("message", "Hello World!");
+        System.out.println("aaaa");
+        return "index";
+    }
+}
